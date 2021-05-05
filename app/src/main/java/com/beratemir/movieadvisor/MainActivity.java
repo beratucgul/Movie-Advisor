8a6444bb65f50084f714d6d2a053c42c02bbf109
JBerat <berat.99@hotmail.com>
2021-05-05 12:18:24 +0300

    username and password input added

--- app/src/main/java/com/beratemir/movieadvisor/MainActivity.java
+++ app/src/main/java/com/beratemir/movieadvisor/MainActivity.java
@@ -3,15 +3,20 @@ package com.beratemir.movieadvisor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText usernameText, passwordText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        
+        usernameText = findViewById(R.id.userNameText);
+        passwordText = findViewById(R.id.passwordText);
+
+
         
     }
 }
