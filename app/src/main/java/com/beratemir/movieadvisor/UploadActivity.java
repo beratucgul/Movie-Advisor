f303ae35302c62a94d59fb5c4547ad1f1c6fc155
JBerat <berat.99@hotmail.com>
2021-05-05 15:21:57 +0300

    upload activity added

---
+++ app/src/main/java/com/beratemir/movieadvisor/UploadActivity.java
@@ -0,0 +1,15 @@
+package com.beratemir.movieadvisor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+
+    }
+}
